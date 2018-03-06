2243d1e48024d72c3449b269b73ea5eed5760552
smmakowski <makowski.sm@gmail.com>
2018-03-05 22:50:00 -0800

    add empty prefabs; download assets; add assets in PreLoad

--- scripts/states/BootState.js
+++ scripts/states/BootState.js
@@ -2,19 +2,21 @@ MyGame = MyGame || {};
 
 MyGame.BootState = {
   init: function() {
+    // set white background color
+    this.game.stage.backgroundColor = '#fff';
+    // scaling and centering options
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignVertically = true;
     this.scale.pageAlignHorizontally = true;
   },
   preload: function() {
     //preload assets for PreloadState
-    this.load.image('loadingBar', '../../assets/images/bar.png');
+    this.load.image('loadingBar', '../../assets/images/preloader-bar.png');
 
   },
   create: function() {
     //set stage background as white, start PreloadState
     console.log('Now in Bootstate');
-    this.game.stage.backgroundColor = '#fff';
     this.state.start('PreloadState');
   }
 }
--- scripts/states/PreloadState.js
+++ scripts/states/PreloadState.js
@@ -1,13 +1,22 @@
 MyGame = MyGame || {};
 
-MyGame.PreloadState ={ 
+MyGame.PreloadState ={
   preload: function() {
     // add sprites for logos and loading bar
     this.loadingBar = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loadingBar');
     this.loadingBar.anchor.setTo(.5); // center anchor to midddle of bar
     this.load.setPreloadSprite(this.loadingBar); // crops arg image depening on load percentage
     // preload assets here
-    this.load.image('background', '../../assets/images/sample-background.png');
+    this.load.image('background', '../../assets/images/backyard2.png');
+    this.load.image('block1', '../../assets/images/bean_blue.png');
+    this.load.image('block2', '../../assets/images/bean_green.png');
+    this.load.image('block3', '../../assets/images/bean_orange.png');
+    this.load.image('block4', '../../assets/images/bean_pink.png');
+    this.load.image('block5', '../../assets/images/bean_purple.png');
+    this.load.image('block6', '../../assets/images/bean_yellow.png');
+    this.load.image('block7', '../../assets/images/bean_red.png');
+    this.load.image('block8', '../../assets/images/bean_white.png');
+    this.load.image('deadBlock', '../../assets/images/bean_dead.png');
   },
   create: function() {
     console.log('Now in PreloadState');
