49c293f43dc5b2a4de91412bab437af9680c3eef
smmakowski <makowski.sm@gmail.com>
2018-03-21 20:20:40 -0700

    migrate static/assets/scripts to public dir

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 // use folder f
-app.use(express.static(`${__dirname}/../`));
+app.use(express.static(`${__dirname}/../public/`));
 
 // start server
 app.listen(PORT, () => {
